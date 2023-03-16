9ac54a06040f3bfad061b00896e22497d5982c2e
Sebastian009w <ia3266595@gmail.com>
2023-03-15 23:05:55 -0500

    network profile

---
+++ components/Network.tsx
@@ -0,0 +1,12 @@
+import { Github } from "./Github";
+
+export const Network = () => {
+  return (
+    <>
+      <a href="https://github.com/JohanSebastiaCG">
+        <Github />
+      </a>
+      <a href=""></a>
+    </>
+  );
+};
