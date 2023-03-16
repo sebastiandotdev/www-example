a478e327e7ede5d130703f11af051302dfb38cd3
Sebastian009w <ia3266595@gmail.com>
2023-03-15 22:47:59 -0500

    about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sebastian</title>
+        <title>Home - Sebastian Castro</title>
         <meta name="description" content="I'm Sebastian" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/roar.jpg" />
@@ -16,11 +16,25 @@ export default function Home() {
             <Image src="/roar.jpg" alt="sebastian" width={200} height={200} />
           </div>
           <div>
-            <h4>Hello I'm</h4>
+            <h4>Hello Im</h4>
             <h1>Sebastian</h1>
           </div>
         </div>
-        <div></div>
+        <div>
+          <p>
+            I am a developer and
+            <a href="https://github.com/JohanSebastiaCG">
+              lover of open source.
+            </a>
+            I like Rust, Go, TypeScript, and quick websites. I build some other
+            open source libraries. Probably reading some web specification right
+            now.
+          </p>
+          <p>
+            Wanna talk about something? <span>Message me on email me at</span>
+            johancs@gmail.com
+          </p>
+        </div>
       </main>
     </>
   );
