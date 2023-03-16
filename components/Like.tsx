0233e6e22bd77468b5bf0ca3d279e0dc482e0c7d
Johan Sebastián Castro <116913262+JohanSebastiaCG@users.noreply.github.com>
2023-03-16 18:33:54 -0500

    Update Like.tsx

--- components/Like.tsx
+++ components/Like.tsx
@@ -6,7 +6,7 @@ export const Like = () => {
         href="https://github.com/JohanSebastiaCG"
         className="text-blue-600 hover:text-blue-500 hover:underline transition duration-75 ease-in-out"
       >
-        open source.
+         open source.
       </a>
       I like Rust, TypeScript, and quick websites. I build libraries. Im
       probably thinking about what to develop.
