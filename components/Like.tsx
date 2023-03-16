5c18bd237fda1e08f5430838368b819cdaf77087
Sebastian009w <ia3266595@gmail.com>
2023-03-16 12:05:34 -0500

    profile person

--- components/Like.tsx
+++ components/Like.tsx
@@ -1,12 +1,12 @@
 export const Like = () => {
   return (
     <p>
-      I am a developer and{" "}
+      I am a developer and lover of
       <a
         href="https://github.com/JohanSebastiaCG"
         className="text-blue-600 hover:text-blue-500 hover:underline transition duration-75 ease-in-out"
       >
-        lover of open source.
+        open source.
       </a>
       I like Rust, TypeScript, and quick websites. I build libraries. Im
       probably thinking about what to develop.
