fd55c82b6f5b8aa72393d761239f2513cc67bd4c
Johan Sebastián Castro <116913262+sebastian009w@users.noreply.github.com>
2023-03-18 19:16:33 -0500

    Update Like.tsx

--- components/Like.tsx
+++ components/Like.tsx
@@ -1,7 +1,7 @@
 export const Like = () => {
   return (
     <p>
-      I am a developer and lover of <a href="https://github.com/JohanSebastiaCG" className="text-blue-600 hover:text-blue-500 hover:underline transition duration-75 ease-in-out"> open source. </a> 
+      I am a developer and lover of <a href="https://github.com/sebastian009w" className="text-blue-600 hover:text-blue-500 hover:underline transition duration-75 ease-in-out"> open source. </a> 
       I like Rust, TypeScript, and quick websites. I build libraries. Im
       probably thinking about what to develop.
     </p>
