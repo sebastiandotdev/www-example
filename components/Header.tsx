a41e3d9fd3480437bc1d6027e16dd3b25c3e653a
Sebastian009w <ia3266595@gmail.com>
2023-03-26 13:40:00 -0500

    profile

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
       <title>Home - Sebastian Castro</title>
       <meta name="description" content="I'm Sebastian" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/roar.jpg" />
+      <link rel="icon" href="https://i.postimg.cc/bvcBY2vY/Whats-App-Image-2023-03-26-at-1-36-05-PM.jpg" />
     </Head>
   );
 };
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Photo = () => {
   return (
     <div>
-      <Image className="w-24 md:w-32 h-24 md:h-32 rounded-full" src="/roar.jpg" alt="sebastian" width={200} height={200} />
+      <Image className="w-24 md:w-32 h-24 md:h-32 rounded-full" src="https://i.postimg.cc/bvcBY2vY/Whats-App-Image-2023-03-26-at-1-36-05-PM.jpg" alt="sebastian" width={200} height={200} />
     </div>
   );
 };
