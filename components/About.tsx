89ddbd1f348fb4b33dda73b025822dfcaeb58f75
Johan Sebastián Castro <116913262+sebastian009w@users.noreply.github.com>
2023-03-31 22:50:37 -0500

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return (
     <div>
-      <h4 className="leading-tight text-gray-900 text-2xl md:text-3xl">Hello I'm</h4>
+      <h4 className="leading-tight text-gray-900 text-2xl md:text-3xl">Hello Im</h4>
       <h1 className="leading-tight text-gray-900 text-4xl md:text-5xl font-semibold">Sebastian</h1>
     </div>
   );
