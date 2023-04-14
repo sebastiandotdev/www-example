187a4be3bad9163a12f7eda362d090c5428d4a1f
sebastian009w <116913262+sebastian009w@users.noreply.github.com>
2023-04-13 21:32:04 -0500

    Update Photo.tsx

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Photo = () => {
   return (
     <div>
-      <Image className="w-24 md:w-32 h-24 md:h-32 rounded-full" src="/sebastian.jpg" alt="sebastian" width={200} height={200} />
+      <Image className="w-24 md:w-32 h-24 md:h-32 rounded-full" src="https://i.postimg.cc/Hkk5ThFL/Whats-App-Image-2023-04-13-at-11-05-29-AM.jpg" alt="sebastian" width={200} height={200} />
     </div>
   );
 };
